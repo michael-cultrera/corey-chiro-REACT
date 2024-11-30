bd721a0d00c5f03f90bddf6c4343da1097ffb84b
michael-cultrera <michaelcultrera0@gmail.com>
2024-11-30 13:46:38 -0500

    Add OutlinedButton component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
-import {Box} from "@mui/material";
+import { Box} from "@mui/material";
+// import CallIcon from '@mui/icons-material/Call';
 import theme from "./theme";
 import { Helmet } from "react-helmet";
 import Navbar from "./components/Navbar";
 import Logo from "./components/Logo";
+import OutlinedButton from "./components/OutlinedButton";
 import "./index.css";
 import HomePage from "./pages/HomePage";
 import ContactUs from "./pages/ContactUs";
@@ -21,8 +23,20 @@ export default function RootLayout({ children }) {
       <body>
         <Router>
           <ThemeProvider theme={theme}>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Logo width={"160px"}/>
+          <Box sx={{ 
+            display: 'flex', 
+            // justifyContent: 'space-between',
+            alignItems: 'center', 
+            width: '100%'
+          }}>
+            <Box sx={{ flex: 1 }} /> {/* Spacer */}
+            <Box sx={{ display: 'flex', justifyContent: 'center', flex: 1 }}>
+              <Logo width="160px" />
+            </Box>
+            <Box sx={{ flex: 1, display: 'flex', }}>
+              <OutlinedButton buttonText="Book Appointment"/>
+              <OutlinedButton buttonText="416-497-8585"/>
+            </Box>
           </Box>
             <Navbar />
             {children}
---
+++ src/components/OutlinedButton.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Typography } from "@mui/material";
+
+export default function OutlinedButton({ buttonText, onClick }) {
+  return (
+    <Button
+        onClick={onClick}
+        variant="outlined"
+        sx={{
+            flexDirection: "column",
+            textTransform: "none",
+            color: "black",
+            borderRadius: "10px",
+            borderColor: "black",
+            width: "180px",
+            transition: "background-color 0.3s ease, color 0.3s ease",
+            '&:hover': {
+            borderColor: "black",
+            backgroundColor: "black",
+            color: "var(--honeydew)"
+            }
+        }}
+        >
+        <Typography variant="body">{buttonText}</Typography>
+    </Button>
+  );
+}
